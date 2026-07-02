a139974f5a930fc6e38ba1ccb799b9dc137714c5
SdKS454 <67646852+SdKS454@users.noreply.github.com>
2026-07-02 14:59:36 +0300

    Task 3

---
+++ Schema.sql
@@ -0,0 +1,5 @@
+insert into Product(name, cost) values('Iphone', 100);
+insert into Product(name, cost) values('Samsung', 150);
+insert into Product(name, cost) values('Ксяоми', 10);
+SELECT * FROM public.product
+drop table Product;
